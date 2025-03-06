65158a2e57d92e1505844ed0041902a2ccf8b160
seanchen810gmailcom <seanchen810@gmail.com>
2025-03-06 14:49:03 +0800

    class1

--- class1/class1.py
+++ class1/class1.py
@@ -1,1 +1,22 @@
+# 這個是單行註解
+# CTRL+?可以切換滑鼠框選的範圍變成註解
+"""
+這個是多行註解1
+這個是多行註解2
+這個事多行註解3
+"""
 
+print("helo From prj1.py")  # 這是把字串印出來的指令
+
+# 基本型態 basic types
+print(1)  # 整數 int
+print(1.1)  # 浮點數 float,只是把字串印出來指令
+print("hello")  # 字串 str,用雙引號""或單引號''包起來都是字串
+print(True)  # 布林值 bool,第一個字串要大寫
+print(None)  # 空值 None Type
+
+
+# 變數 varilble
+a = 1  # 把1這個變數存到電腦的記憶體中,並且取名為a
+# =的功能是將右邊的資料存到左邊的變數空間當中
+print(a)  # 把a這變數的質印出來
