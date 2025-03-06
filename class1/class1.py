874fa0d0d1130af3d67ede8fdb5826b3752a5014
seanchen810gmailcom <seanchen810@gmail.com>
2025-03-06 14:53:01 +0800

    git test

--- class1/class1.py
+++ class1/class1.py
@@ -1,22 +1,21 @@
 # 這個是單行註解
-# CTRL+?可以切換滑鼠框選的範圍變成註解
+# command+?可以切換滑鼠框選的範圍變成註解
 """
 這個是多行註解1
 這個是多行註解2
 這個事多行註解3
 """
 
-print("helo From prj1.py")  # 這是把字串印出來的指令
+print("hello From class1.py")  # 這是把字串印出來的指令
 
 # 基本型態 basic types
 print(1)  # 整數 int
-print(1.1)  # 浮點數 float,只是把字串印出來指令
+print(1.1)  # 浮點數 float
 print("hello")  # 字串 str,用雙引號""或單引號''包起來都是字串
 print(True)  # 布林值 bool,第一個字串要大寫
-print(None)  # 空值 None Type
-
+print(None)  # 空值 None 代表沒有資料
 
 # 變數 varilble
-a = 1  # 把1這個變數存到電腦的記憶體中,並且取名為a
-# =的功能是將右邊的資料存到左邊的變數空間當中
-print(a)  # 把a這變數的質印出來
+a = 1  # 把1這個數字存到電腦的記憶體中,並且取名為a
+# = 的功能是將右邊的資料存到左邊的變數空間當中, 也叫做賦值
+print(a)  # 把變數a裡面存的資料印出來
