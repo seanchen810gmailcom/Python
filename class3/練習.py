9c70724da51f0e03ea3cee0ca71ab4882bd1986f
seanchen810gmailcom <seanchen810@gmail.com>
2025-03-15 12:06:16 +0800

    class3

--- class3/練習.py
+++ class3/練習.py
@@ -7,4 +7,15 @@ else:
     print("密碼錯誤")
     print("BBBBBBBBBBBBBBBB")
 
-成績
+成績1 = input("請輸入成績:")
+成績2 = int(成績1)
+if 成績2 >= 90:
+    print("A")
+elif 成績2 >= 80:
+    print("B")
+elif 成績2 >= 70:
+    print("C")
+elif 成績2 >= 60:
+    print("D")
+else:
+    print("F")
