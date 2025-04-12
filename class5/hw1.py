238ef7903d11cf0b1dd781c06c2e370e212cc267
seanchen810gmailcom <seanchen810@gmail.com>
2025-04-12 15:01:26 +0800

    class5

--- class5/hw1.py
+++ class5/hw1.py
@@ -3,3 +3,16 @@
 讓turtle蓋出一個時鐘數字的位置
 總共會蓋出12個印章
 """
+
+import turtle as t
+
+a = 0
+for i in range(8):
+    t.speed(0)
+    t.penup()
+    t.forward(100)
+    t.stamp()
+    t.home()
+    a = a + 1
+    t.right(45 * a)
+t.done()
--- class5/hw2.py
+++ class5/hw2.py
@@ -1,3 +1,20 @@
 """
 使用turtle 繪製時鐘
 """
+
+import turtle as t
+import time
+
+a = 0
+t.speed(0)
+t.left(90)
+for i in range(61):
+    t.forward(100)
+    t.home()
+    t.clear()
+    time.sleep(1)
+    a = int(a + 1)
+    b = int(6 * a)
+    t.left(90 - b)
+
+t.done()
