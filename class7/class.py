62831568731705c9a268d5e08ed33004ec73e71d
seanchen810gmailcom <seanchen810@gmail.com>
2025-04-29 11:36:30 +0800

    class7

---
+++ class7/class.py
@@ -0,0 +1,17 @@
+# for ... else
+# 當for迴圈正常結束時，執行else區塊的程式
+# example
+for i in range(5):
+    print(i)
+else:
+    print("for迴圈正常結束")
+
+# while ... else
+# 當while回圈正常結束時，執行else區塊的程式
+# example
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("while回圈正常結束")
